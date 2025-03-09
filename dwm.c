bf525373f22893ad370b98ccec5bd68b491f5b89
dvlcx <ave.burning.goat.666@gmail.com>
2025-03-09 23:29:51 +0300

    Revert "quitprompt applied"
    This reverts commit 2b7dc4cb0cf4dbc44d06000df919ffee4edff553.

--- dwm.c
+++ dwm.c
@@ -187,7 +187,6 @@ static Client *nexttiled(Client *c);
 static void pop(Client *c);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
-static void quitprompt(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
@@ -261,7 +260,6 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 };
 static Atom wmatom[WMLast], netatom[NetLast];
 static int running = 1;
-static int restart = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
@@ -1262,31 +1260,6 @@ quit(const Arg *arg)
 	running = 0;
 }
 
-void
-quitprompt(const Arg *arg)
-{
-	FILE *pp = popen("echo -e \"no\nrestart\nyes\" | dmenu -i -sb red -p \"Quit DWM?\"", "r");
-	if(pp != NULL) {
-		char buf[1024];
-		if (fgets(buf, sizeof(buf), pp) == NULL) {
-			fprintf(stderr, "Quitprompt: Error reading pipe!\n");
-			return;
-		}
-		if (strcmp(buf, "yes\n") == 0) {
-			pclose(pp);
-			restart = 0;
-			quit(NULL);
-		} else if (strcmp(buf, "restart\n") == 0) {
-			pclose(pp);
-			restart = 1;
-			quit(NULL);
-		} else if (strcmp(buf, "no\n") == 0) {
-			pclose(pp);
-			return;
-		}
-	}
-}
-
 Monitor *
 recttomon(int x, int y, int w, int h)
 {
@@ -2190,8 +2163,5 @@ main(int argc, char *argv[])
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
-	if (restart == 1) {
-		execlp("dwm", "dwm", NULL);
-	}
 	return EXIT_SUCCESS;
 }
